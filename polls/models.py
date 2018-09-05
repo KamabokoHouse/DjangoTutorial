4526374d553899e4f011b3352b41a91f85eb60bd
shoma <nerorubymoon@gmail.com>
2018-09-05 16:32:57 +0900

    django tutorial3 complete

--- polls/models.py
+++ polls/models.py
@@ -1,3 +1,6 @@
+"""
+モデルクラス
+"""
 import datetime
 
 from django.db import models
@@ -8,18 +11,29 @@ from django.utils import timezone
 
 # modelはmodels.Modelのサブクラスとして作成します
 class Question(models.Model):
+    """
+    questionモデル
+    """
     question_text = models.CharField(max_length=200)
-    # 第一引数には人間用の説明を含められる（省略可能）
     pub_date = models.DateTimeField('date published')
     def __str__(self):
         return self.question_text
     def was_published_recently(self):
+        """
+        return pubdata
+        """
         return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
 
 class Choice(models.Model):
-    # 外部キーでリレーションしてる。Question : Choice 多対１
-    question = models.ForeignKey(Question,on_delete=models.CASCADE)
+    """
+    Choceモデル
+    """
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
     def __str__(self):
+        """
+        テキストを返します
+        """
         return self.choice_text
+        
